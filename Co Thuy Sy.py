8c8745cd1ce539d0088ad496716cb74a6233e80d
baobinh222 <96453548+baobinh222@users.noreply.github.com>
2022-07-18 12:04:53 +0700

    Add files via upload

---
+++ Co Thuy Sy.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import turtle
+#turtle = Turtle()
+t = turtle.Turtle()
+#t.shape('Turtle')
+t.color('red')
+
+t.fillcolor('red')
+t.begin_fill()
+t.forward(30)
+t.right(90)
+t.forward(30)
+t.left(90)
+t.forward(30)
+t.right(90)
+t.forward(30)
+t.right(90)
+t.forward(30)
+t.left(90)
+t.forward(30)
+t.right(90)
+t.forward(30)
+t.right(90)
+t.forward(30)
+t.left(90)
+t.forward(30)
+t.right(90)
+t.forward(30)
+t.right(90)
+t.forward(30)
+t.left(90)
+t.forward(30)
+t.end_fill()
+
+t.penup()
+t.forward(50)
+t.pendown()
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(190)
+t.right(90)
+t.forward(270)
+t.right(90)
+t.forward(190)
+t.right(90)
+t.forward(120)
+
+turtle.done()
+
+
+# In[ ]:
+
+
+
+
